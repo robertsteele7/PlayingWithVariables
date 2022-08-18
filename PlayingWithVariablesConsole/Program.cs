efa02b5c8551dc1e83645c694699985ef64be97e
robertsteele7 <atpcycle7@gmail.com>
2022-08-18 10:45:22 -0500

    first commit

---
+++ PlayingWithVariablesConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PlayingWithVariablesConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable Declarations
+           
+            string name;
+            int age;
+            char dataType;
+            bool isAnnoyed;
+            double y = 4.3;
+            decimal x = 2.1m;
+            //Variable initializations
+            age = 53;
+            name = "Rob";
+            dataType = 'A';
+            isAnnoyed = true;
+
+
+
+
+
+            Console.WriteLine($"{name} {age} {dataType} Software engineer. is he annoyed: {isAnnoyed}");
+        }
+    }
+}
